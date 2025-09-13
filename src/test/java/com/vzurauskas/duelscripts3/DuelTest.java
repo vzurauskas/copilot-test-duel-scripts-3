3eeeaab47ea30f4e9e28a3d38912ea7f92d8d9ea
Vytautas Žurauskas <zurauskas.vytautas@gmail.com>
2025-09-13 18:55:40 +0300

    Extract description tests.

--- src/test/java/com/vzurauskas/duelscripts3/DuelTest.java
+++ src/test/java/com/vzurauskas/duelscripts3/DuelTest.java
@@ -28,31 +28,7 @@ public final class DuelTest {
         assertTrue(description.intValue("damage") > 0);
     }
 
-    @Test
-    void parryNegationRecordedAsZeroDamage() {
-        CombatScript aliceScript = new FixedScript()
-            .parry(Fighter::torso)
-            .strike(Fighter::head);
-        CombatScript bobScript = new FixedScript()
-            .parry(Fighter::head)
-            .strike(Fighter::legs);
-
-        FightHistory history = new FightHistory();
-        Fighter alice = new Fighter("Alice", aliceScript, history);
-        Fighter bob = new Fighter("Bob", bobScript, history);
-        Arena arena = new Arena(alice, bob, history);
-
-        arena.nextTurn();
-
-        assertEquals(
-            """
-            Turn 1:
-                Alice parry=torso, strike=head [parried], damage=0
-                Bob parry=head, strike=legs [hit], damage=2
-            """,
-            history.describeTurn(1)
-        );
-    }
+    // moved to FightDescriptionTest
 
     @Test
     void exposesRecentOpponentParryForScript() {
@@ -136,59 +112,10 @@ public final class DuelTest {
         assertEquals(2, aragorn.legs().damage());
     }
 
-    @Test
-    void humanReadableSummaryRendersTwoTurns() {
-        CombatScript aliceScript = new FixedScript()
-            .parry(Fighter::torso)
-            .strike(Fighter::head);
-        CombatScript bobScript = new FixedScript()
-            .parry(Fighter::head)
-            .strike(Fighter::legs);
+    // moved to FightDescriptionTest
 
-        FightHistory history = new FightHistory();
-        Fighter alice = new Fighter("Alice", aliceScript, history);
-        Fighter bob = new Fighter("Bob", bobScript, history);
-        Arena arena = new Arena(alice, bob, history);
-
-        arena.nextTurn();
-        arena.nextTurn();
-
-        assertEquals(
-            """
-            Turn 1:
-                Alice parry=torso, strike=head [parried], damage=0
-                Bob parry=head, strike=legs [hit], damage=2
-            Turn 2:
-                Alice parry=torso, strike=head [parried], damage=0
-                Bob parry=head, strike=legs [hit], damage=2
-            """,
-            history.describeTurn(1) + history.describeTurn(2)
-        );
-    }
-    @Test
-    void recordsTurnWithParriesTargetsAndOutcomes() {
-        CombatScript aliceScript = new FixedScript()
-            .parry(Fighter::torso)
-            .strike(Fighter::head);
-        CombatScript bobScript = new FixedScript()
-            .parry(Fighter::head)
-            .strike(Fighter::legs);
-
-        FightHistory history = new FightHistory();
-        Fighter alice = new Fighter("Alice", aliceScript, history);
-        Fighter bob = new Fighter("Bob", bobScript, history);
-        Arena arena = new Arena(alice, bob, history);
-        arena.nextTurn();
-
-        assertEquals(
-            """
-            Turn 1:
-                Alice parry=torso, strike=head [parried], damage=0
-                Bob parry=head, strike=legs [hit], damage=2
-            """,
-            history.describeTurn(1)
-        );
-    }
+    // moved to FightDescriptionTest
+    // moved to FightDescriptionTest
 }
 
 
---
+++ src/test/java/com/vzurauskas/duelscripts3/FightDescriptionTest.java
@@ -0,0 +1,115 @@
+package com.vzurauskas.duelscripts3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class FightDescriptionTest {
+
+    @Test
+    void recordsTurnWithParriesTargetsAndOutcomes() {
+        CombatScript aliceScript = new FixedScript()
+            .parry(Fighter::torso)
+            .strike(Fighter::head);
+        CombatScript bobScript = new FixedScript()
+            .parry(Fighter::head)
+            .strike(Fighter::legs);
+
+        FightHistory history = new FightHistory();
+        Fighter alice = new Fighter("Alice", aliceScript, history);
+        Fighter bob = new Fighter("Bob", bobScript, history);
+        Arena arena = new Arena(alice, bob, history);
+        arena.nextTurn();
+
+        assertEquals(
+            """
+            Turn 1:
+                Alice parry=torso, strike=head [parried], damage=0
+                Bob parry=head, strike=legs [hit], damage=2
+            """,
+            history.describeTurn(1)
+        );
+    }
+
+    @Test
+    void parryNegationRecordedAsZeroDamage() {
+        CombatScript aliceScript = new FixedScript()
+            .parry(Fighter::torso)
+            .strike(Fighter::head);
+        CombatScript bobScript = new FixedScript()
+            .parry(Fighter::head)
+            .strike(Fighter::legs);
+
+        FightHistory history = new FightHistory();
+        Fighter alice = new Fighter("Alice", aliceScript, history);
+        Fighter bob = new Fighter("Bob", bobScript, history);
+        Arena arena = new Arena(alice, bob, history);
+
+        arena.nextTurn();
+
+        assertEquals(
+            """
+            Turn 1:
+                Alice parry=torso, strike=head [parried], damage=0
+                Bob parry=head, strike=legs [hit], damage=2
+            """,
+            history.describeTurn(1)
+        );
+    }
+
+    @Test
+    void humanReadableSummaryRendersTwoTurns() {
+        CombatScript aliceScript = new FixedScript()
+            .parry(Fighter::torso)
+            .strike(Fighter::head);
+        CombatScript bobScript = new FixedScript()
+            .parry(Fighter::head)
+            .strike(Fighter::legs);
+
+        FightHistory history = new FightHistory();
+        Fighter alice = new Fighter("Alice", aliceScript, history);
+        Fighter bob = new Fighter("Bob", bobScript, history);
+        Arena arena = new Arena(alice, bob, history);
+
+        arena.nextTurn();
+        arena.nextTurn();
+
+        assertEquals(
+            """
+            Turn 1:
+                Alice parry=torso, strike=head [parried], damage=0
+                Bob parry=head, strike=legs [hit], damage=2
+            Turn 2:
+                Alice parry=torso, strike=head [parried], damage=0
+                Bob parry=head, strike=legs [hit], damage=2
+            """,
+            history.describeTurn(1) + history.describeTurn(2)
+        );
+    }
+
+    @Test
+    void appliesDamageMultiplierAndRecordsDamage() {
+        CombatScript aliceScript = new FixedScript()
+            .parry(Fighter::legs)
+            .strike(Fighter::head); // expect hit to head => damage 5
+        CombatScript bobScript = new FixedScript()
+            .parry(Fighter::torso)
+            .strike(Fighter::torso); // expect hit to torso => damage 3
+
+        FightHistory history = new FightHistory();
+        Fighter alice = new Fighter("Alice", aliceScript, history);
+        Fighter bob = new Fighter("Bob", bobScript, history);
+        Arena arena = new Arena(alice, bob, history);
+
+        arena.nextTurn();
+
+        assertEquals(
+            """
+            Turn 1:
+                Alice parry=legs, strike=head [hit], damage=5
+                Bob parry=torso, strike=torso [hit], damage=3
+            """,
+            history.describeTurn(1)
+        );
+    }
+}
